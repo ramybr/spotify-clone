fe246be3114fdc183c28f0c21ab88a8ddfcc7faf
ramybr <ramybr2903@gmail.com>
2024-02-24 13:12:34 +0100

    final

---
+++ hooks/useLoadSongUrl.ts
@@ -0,0 +1,21 @@
+import { Song } from "@/types";
+import {
+  useSessionContext,
+  useSupabaseClient,
+} from "@supabase/auth-helpers-react";
+
+const useLoadSongUrl = (song: Song) => {
+  const { supabaseClient } = useSessionContext();
+
+  if (!song) {
+    return "";
+  }
+
+  const { data: songData } = supabaseClient.storage
+    .from("songs")
+    .getPublicUrl(song.song_path);
+
+  return songData.publicUrl;
+};
+
+export default useLoadSongUrl;
